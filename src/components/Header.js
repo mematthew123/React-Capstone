99c641573dd3b46705adc6b96bcee0fdcc4ef5e4
matthew <mematthew123@gmail.com>
2021-06-04 19:16:12 -0600

    update to login

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Login from './LoginComponent';
+//import Login from './LoginComponent';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
@@ -18,18 +18,69 @@ class Header extends Component {
                     <button className="btn btn-outline-primary my-2 my-sm-0" data-toggle="modal" data-target="#exampleModal">Login</button>
                 </nav>
 
+
+
+
+
                 <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div className="modal-dialog" role="document">
                         <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalLabel">Welcome Back</h5>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
                             <div className="modal-body">
-                                <Login parent={this.props.parent} />
+
+
+                                <div class="form-outline">
+                                    <input type="text" id="formControlDefault" class="form-control" />
+                                    <label class="form-label" for="formControlDefault">Email address </label>
+                                </div>
+
+
+                                <div class="form-outline">
+                                    <input type="text" id="formControlDefault" class="form-control" />
+                                    <label class="form-label" for="formControlDefault">Password</label>
+                                </div>
+
+
+                                          ...
+
+                                </div>
+                            <div className="modal-footer">
+
+
+
+                                <button type="button" className="btn btn-primary" data-dismiss="modal">Login</button>
+                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Nah'</button>
+
                             </div>
                         </div>
                     </div>
                 </div>
+
+
             </div>
         );
     }
 }
 
 export default Header;
+
+
+
+
+// <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+// <div className="modal-dialog" role="document">
+//     <div className="modal-content">
+//         <div className="modal-body">
+
+
+
+//             <Login parent={this.props.parent} />
+//         </div>
+//     </div>
+// </div>
+// </div>
