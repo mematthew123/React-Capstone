8e51958a66e417cff60ce3eebeb3602fcb0d3f2d
matthew <mematthew123@gmail.com>
2021-06-07 23:38:58 -0600

    Progress on Contact Form and footer- needs completetion of states

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,70 @@
 import React, { Component } from "react";
-import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { NavLink, withRouter } from 'react-router-dom';
+import { login } from '../Actions/Actions';
+import * as ActionTypes from '../Actions/Types';
+import ContactForm from './ContactForm'
 
 class Footer extends Component {
 
     constructor(props) {
         super(props);
 
+    }
 
+    handleChange(event) {
+
+        switch (event.target.type) {
+            
+        }
+
+    }
+
+    handleClick(event) {
+        
+
+        
     }
 
     render() {
         return (
-    <nav className="navbar fixed-bottom navbar-light bg-white">
-    <a className="navbar-brand" href="#"></a>
+            <div>
+            <nav className="navbar fixed-bottom navbar-light bg-white">
+            <button className="btn btn-outline-primary my-2 my-sm-0" data-toggle="modal" data-target="#exampleModal">Login/Sign Up</button>
+                </nav>
 
 
-    </nav>
+                <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalLabel">Login / Sign Up</h5>
+                            </div>
+                            <div className="modal-body">
+                                <div className="form-outline">
+                                    <input type="email" className="form-control" value={this.state.email} onChange={this.handleChange} />
+                                    <label className="form-label" htmlFor="formControlDefault">Email address </label>
+                                </div>
+                                <div className="form-outline">
+                                    <input type="password" className="form-control" value={this.state.password} onChange={this.handleChange} />
+                                    <label className="form-label" htmlFor="formControlDefault">Password</label>
+                                </div>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-primary" data-dismiss="modal" onClick={this.handleClick}>Login</button>
+                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
-    }       
+    }
 }
+
+const mapDispatchToProps = dispatch => ({
+    login: (, ) => dispatch(login(, ))
+});
 export default Footer;
 
 
