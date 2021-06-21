5c98c279b0f50136d3bf8629a89ee291f3648647
matthew <mematthew123@gmail.com>
2021-06-21 14:08:44 -0600

    changed class to ClassName

--- src/components/TransferModal/Transfer.js
+++ src/components/TransferModal/Transfer.js
@@ -13,42 +13,42 @@ class Transfer extends Component {
 }
 
 return (
-  <div class="modal" tabindex="-1">
-    <div class="modal-dialog" >
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title">Modal title</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+  <div className="modal" tabindex="-1">
+    <div className="modal-dialog" >
+      <div className="modal-content">
+        <div className="modal-header">
+          <h5 className="modal-title">Modal title</h5>
+          <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
-        <div class="modal-body">
+        <div className="modal-body">
 
         <p>Transfer From...</p>
 
-          <div class="dropdown">
-            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false"> Dropdown button </button>
+          <div className="dropdown">
+            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false"> Dropdown button </button>
             <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-              <li><a class="dropdown-item" href="#">Checking</a></li>
-              <li><a class="dropdown-item" href="#">Savings</a></li>
-              <li><a class="dropdown-item" href="#">IRA ish..</a></li>
+              <li><a className="dropdown-item" href="#">Checking</a></li>
+              <li><a className="dropdown-item" href="#">Savings</a></li>
+              <li><a className="dropdown-item" href="#">IRA ish..</a></li>
             </ul>
           </div>
 
           <p>Transfer Too..</p>
 
-          <div class="dropdown">
-          <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false"> Dropdown button </button>
-          <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton2">
-          <li><a class="dropdown-item" href="#">Checking</a></li>
-          <li><a class="dropdown-item" href="#">Savings</a></li>
-          <li><a class="dropdown-item" href="#">IRA ish..</a></li>
+          <div className="dropdown">
+          <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false"> Dropdown button </button>
+          <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton2">
+          <li><a className="dropdown-item" href="#">Checking</a></li>
+          <li><a className="dropdown-item" href="#">Savings</a></li>
+          <li><a className="dropdown-item" href="#">IRA ish..</a></li>
           </ul>
         </div>
 
         
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
+          <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" className="btn btn-primary">Save changes</button>
         </div>
       </div>
     </div>
@@ -56,15 +56,15 @@ return (
 
 
     <div class="btn-group">
-      <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <button type="button" className="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         Action
             </button>
-      <div class="dropdown-menu">
-        <a class="dropdown-item" href="#">Action</a>
-        <a class="dropdown-item" href="#">Another action</a>
-        <a class="dropdown-item" href="#">Something else here</a>
-        <div class="dropdown-divider"></div>
-        <a class="dropdown-item" href="#">Separated link</a>
+      <div className="dropdown-menu">
+        <a className="dropdown-item" href="#">Action</a>
+        <a className="dropdown-item" href="#">Another action</a>
+        <a className="dropdown-item" href="#">Something else here</a>
+        <div className="dropdown-divider"></div>
+        <a className="dropdown-item" href="#">Separated link</a>
       </div>
     </div>
   </div>
